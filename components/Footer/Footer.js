8136f0546d751ca37f2f5574afcd52e9bced4130
aritroCoder <ogcodinggod@gmail.com>
2023-01-27 15:00:47 +0530

    needed bug fix

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -44,7 +44,7 @@ const Footer = () => {
         </div>
         <div className={[styles.sectionSeperator].join(" ")}></div>
         <div className={[styles.section1].join(" ")}>
-          <h4>Email : <a href="">anweshaiitp@gmail.com</a></h4>
+          <h4>Email : <a href="">anweshaiitp@gmail.com</a></h4> 
           <h4>Phone : +91 7907323522 </h4>
           <h4>Address : Anwesha Office,
             Indian Institute of Technology Patna,
